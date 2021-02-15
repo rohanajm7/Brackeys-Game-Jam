6f078343ef3563a8fbb9d58d0041ccb3e361e927
Rohan Ajmera <rohanajm@gmail.com>
2021-02-15 22:17:38 +0530

    Movements

---
+++ Assets/Movement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    [SerializeField] float moveSpeed = 1f;
+    [SerializeField] float jumpSpeed = 1f;
+    float moveX = 0f;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (rb != null)
+        {
+            AddInput();
+        }
+    }
+
+    void AddInput()
+    {
+        if(gameObject.tag == "Player1")
+        {
+            moveX = Input.GetAxis("Horizontal");
+            float jumpY = Input.GetAxis("Jump") * jumpSpeed * Time.deltaTime;
+            float forceX = moveX * moveSpeed * Time.deltaTime;
+            Vector2 force = new Vector2(forceX, jumpY);
+            rb.AddForce(force);
+        }
+        else
+        {
+            moveX = Input.GetAxis("Horizontal1");
+            float jumpY = Input.GetAxis("Jump1") * jumpSpeed * Time.deltaTime;
+            float forceX = moveX * moveSpeed * Time.deltaTime;
+            Vector2 force = new Vector2(forceX, jumpY);
+            rb.AddForce(force);
+        }
+    }
+}
